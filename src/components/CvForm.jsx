1f884ffdd4a52a4bf633414a6c444f08f134c01c
nemanja <antonijevicnemanja68@gmail.com>
2025-01-17 00:25:23 +0100

    FEAT cv form ui

--- src/components/CvForm.jsx
+++ src/components/CvForm.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const CvForm = () => {
   return (
-    <div className="h-auto flex-row shadow-md bg-second">
-        <h1 className='text-center font-bold text-2xl mb-5'>PERSONAL INFO</h1>
+    <div className="flex-row shadow-lg bg-second rounded-lg">
+        <h1 className='text-start font-bold text-2xl mb-5 ml-3'>PERSONAL INFO</h1>
+
         <div className="md:flex flew-row justify-center gap-4 items-center mb-5 m-3">
             <label htmlFor="name">First Name</label>
-            <input className="w-full rounded-lg md:h-6 h-9 bg-primary" name="name" type="text" />
+            <input className="w-full rounded-lg md:h-6 h-9 bg-primary" name="name" type="text"/>
             <label htmlFor="surname">Last Name</label>
             <input className="w-full rounded-lg md:h-6 h-9 bg-primary" name="surname" type="text" />
         </div>
@@ -21,6 +22,34 @@ const CvForm = () => {
         <div className="md:flex flew-row justify-center gap-4 items-center m-3">
             <label htmlFor="address">Address</label>
             <input className="w-full rounded-lg md:h-6 h-9 bg-primary" name="address" type="text" />
+        </div>
+
+        <h1 className='text-start font-bold text-2xl mt-12 mb-5 ml-3'>EDUCATION</h1>
+
+        <div className="md:flex flew-row justify-center gap-4 items-center m-3">
+            <label htmlFor="deggre">Deggre</label>
+            <input className="w-full rounded-lg md:h-6 h-9 bg-primary" name="deggre" type="text" />
+        </div>
+
+        <div className="md:flex flew-row justify-center gap-4 items-center m-3">
+            <label htmlFor="School/Uni">School/Uni</label>
+            <input className="w-full rounded-lg md:h-6 h-9 bg-primary" name="School/Uni" type="text" />
+            <label htmlFor="City">City</label>
+            <input className="w-full rounded-lg md:h-6 h-9 bg-primary" name="City" type="text" />
+        </div>
+
+        <h1 className='text-start font-bold text-2xl mt-12 mb-5 ml-3'>EXPERIENCES</h1>
+
+        <div className="md:flex flew-row justify-center gap-4 items-center m-3">
+            <label htmlFor="Job Title">Job Title</label>
+            <input className="w-full rounded-lg md:h-6 h-9 bg-primary" name="Job Title" type="text" />
+        </div>
+
+        <div className="md:flex flew-row justify-center gap-4 items-center m-3">
+            <label htmlFor="Company">Company</label>
+            <input className="w-full rounded-lg md:h-6 h-9 bg-primary" name="Company" type="text" />
+            <label htmlFor="City">City</label>
+            <input className="w-full rounded-lg md:h-6 h-9 bg-primary" name="City" type="text" />
         </div>
     </div>
   )
