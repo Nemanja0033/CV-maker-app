16422d7e8cb988858cfa703a21e186ae178a7c84
nemanja <antonijevicnemanja68@gmail.com>
2025-01-18 00:43:13 +0100

    FEAT: Preview component

--- src/components/CvForm.jsx
+++ src/components/CvForm.jsx
@@ -5,21 +5,19 @@ import Button from './Button'
 
 const CvForm = () => {
 
-    const [cvData, setCvData] = useState({
-        firstName: '',
-        lastName: '',
-        phone: '',
-        jobTitle: '',
-        address: '',
-        deggre: '',
-        school: '',
-        city1: '',
-        job: '',
-        company: '',
-        city2: '',
-        language: '',
-        level: '',
-    });
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [jobTitle, setJobTitle] = useState('');
+    const [address, setAddress] = useState('');
+    const [deggre, setDeggre ] = useState('');
+    const [school, setSchool] = useState('');
+    const [jobTitle2, setJobTitle2] = useState('');
+    const [company, setCompany] = useState('');
+    const [city, setCity] = useState('');
+    const [city2, setCity2] = useState('');
+    const [language, setLanguage] = useState('');
+    const [level, setLevel] = useState('');
 
 
   return (
@@ -28,59 +26,59 @@ const CvForm = () => {
             <span className='text-start font-bold text-strong text-2xl mb-5 ml-3 flex items-center gap-2'>PERSONAL INFO<CircleUser /></span>
             <Button text={'Preview'} icon={<Eye size={20} />} />
        </div>
-       <h1>{cvData.firstName}</h1>
+
         <div className="md:flex flew-row justify-center gap-4 items-center mb-5 m-3">
             <label htmlFor="name">First Name</label>
-            <Input name={'name'} />
+            <Input onchange={(e) => setFirstName(e.target.value)} name={'name'} />
             <label htmlFor="surname">Last Name</label>
-            <Input name={'surname'} />
+            <Input  onchange={(e) => setLastName(e.target.value)} name={'surname'} />
         </div>
 
         <div className="md:flex flew-row justify-center gap-4 items-center m-3">
             <label htmlFor="phone">Phone</label>
-            <Input name={'phone'} />
+            <Input  onchange={(e) => setPhone(e.target.value)} name={'phone'} />
             <label htmlFor="job">Job Title</label>
-            <Input name={'job'} />
+            <Input  onchange={(e) => setJobTitle(e.target.value)} name={'job'} />
         </div>
 
         <div className="md:flex flew-row justify-center gap-4 items-center m-3">
             <label htmlFor="address">Address</label>
-            <Input name={'address'} />
+            <Input  onchange={(e) => setAddress(e.target.value)} name={'address'} />
         </div>
 
         <h1 className='text-start font-bold text-2xl mt-12 mb-5 ml-3 flex items-center  text-strong gap-2'>EDUCATION <GraduationCap /></h1>
 
         <div className="md:flex flew-row justify-center gap-4 items-center m-3">
             <label htmlFor="deggre">Deggre</label>
-            <Input name={'deggre'} />
+            <Input  onchange={(e) => setDeggre(e.target.value)} name={'deggre'} />
         </div>
 
         <div className="md:flex flew-row justify-center gap-4 items-center m-3">
             <label htmlFor="School/Uni">School/Uni</label>
-            <Input name={'School/Uni'} />
+            <Input  onchange={(e) => setSchool(e.target.value)} name={'School/Uni'} />
             <label htmlFor="City">City</label>
-            <Input name={'City'} />
+            <Input  onchange={(e) => setCity(e.target.value)} name={'City'} />
         </div>
 
         <h1 className='text-start font-bold text-2xl mt-12 mb-5 ml-3 flex items-center  text-strong gap-2'>EXPERIENCES <BriefcaseBusiness /></h1>
 
         <div className="md:flex flew-row justify-center gap-4 items-center m-3">
             <label htmlFor="Job Title">Job Title</label>
-            <Input name={'Job Title'} />
+            <Input  onchange={(e) => setJobTitle2(e.target.value)} name={'Job Title'} />
         </div>
 
         <div className="md:flex flew-row justify-center gap-4 items-center m-3">
             <label htmlFor="Company">Company</label>
-            <Input name={'Company'} />
+            <Input onchange={(e) => setCompany(e.target.value)} name={'Company'} />
             <label htmlFor="City">City</label>
-            <Input name={'City'} />
+            <Input  onchange={(e) => setCity2(e.target.value)} name={'City'} />
         </div>
 
         <div className="md:flex flew-row justify-center gap-4 items-center m-3">
             <label htmlFor="Language">Language</label>
-            <Input name={'Language'} />
+            <Input  onchange={(e) => setLanguage(e.target.value)} name={'Language'} />
             <label htmlFor="level">Level</label>
-            <Input name={'level'} />
+            <Input  onchange={(e) => setLevel(e.target.value)} name={'level'} />
         </div>
     </div>
   )
