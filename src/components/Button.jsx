fb4c3bf81f9e44905128b2fc5130d5c439943231
nemanja <antonijevicnemanja68@gmail.com>
2025-01-19 17:40:43 +0100

    FEAT:: button and input UI/UX features

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Button = ({text, onclick, icon}) => {
   return (
-    <button onClick={onclick} className='w-auto h-8 rounded-lg text-second bg-strong m-1 flex items-center gap-2'>{text}{icon}</button>
+    <button 
+      onClick={onclick} 
+      className='w-auto h-8 rounded-lg text-second bg-strong hover:opacity-90 hover:scale-105 transition-all m-1 flex items-center gap-2'>
+        {text}
+        {icon}
+    </button>
   )
 }
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Input = ({name, onChange, value}) => {
   return (
-    <input value={value} onChange={onChange} className="w-full rounded-lg md:h-6 h-9 bg-primary" name={name} type="text"/>
+    <input 
+      value={value} 
+      onChange={onChange} 
+      className="w-full hover:opacity-90 hover:scale-105 transition-all rounded-lg md:h-6 h-9 bg-primary" 
+      name={name} 
+      type="text"
+    />
   )
 }
 
